d4c6b165e32c1db809b71cf9c049bb5f3f902da6
waterfairy <995637517@qq.com>
2017-08-22 15:06:39 +0800

    pdf

--- app/src/main/java/com/waterfairy/pdfview/MainActivity.java
+++ app/src/main/java/com/waterfairy/pdfview/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private void displayFromFile(File file) {
         PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
         pdfView.fromFile(file)   //设置pdf文件地址
-                .defaultPage(6)         //设置默认显示第1页
+                .defaultPage(1)         //设置默认显示第1页
                 .showMinimap(false)     //pdf放大的时候，是否在屏幕的右上角生成小地图
                 .swipeVertical(true)  //pdf文档翻页是否是垂直翻页，默认是左右滑动翻页
                 .enableSwipe(true)   //是否允许翻页，默认是允许翻
